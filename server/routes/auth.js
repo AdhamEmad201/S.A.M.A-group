e751a67b3759d3edc5af5ebc9486c4157de1ca91
AdhamEmad201 <adhammemadd201@gmail.com>
2026-05-05 17:48:31 +0300

    Delete server directory

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, verifyToken } = require('../controllers/authController');
-const authMiddleware = require('../middleware/auth');
-
-router.post('/login', login);
-router.get('/verify', authMiddleware, verifyToken);
-
-module.exports = router;
